3df02142e97fc9b657d1031f821b00d2d5fc33fd
LuisAlon64 <3luis.alonso3@gmail.com>
2025-05-25 02:26:43 -0500

    a

---
+++ Clase 2/ejercicio 1.py
@@ -0,0 +1,9 @@
+# Filtrar palabras largas, mas de 5 letras
+palabras = ["data", "science", "machine", "learning", "python"]
+
+# Tu código aquí
+for palabra in palabras:
+    if len(palabra) > 5:
+        print(palabra)
+    else:
+        pass
